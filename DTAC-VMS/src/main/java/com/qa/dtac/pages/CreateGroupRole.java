125647f996bd7f3a2ecc68ed8731b834a4f53ed6
Mukul <mukulgera304@gmail.com>
2019-04-03 17:16:52 +0530

    Group role...

---
+++ DTAC-VMS/src/main/java/com/qa/dtac/pages/CreateGroupRole.java
@@ -0,0 +1,103 @@
+package com.qa.dtac.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.qa.dtac.base.BasePage;
+import com.qa.dtac.generic.Utility;
+
+public class CreateGroupRole extends BasePage {
+
+	public CreateGroupRole(WebDriver driver) {
+		super(driver);
+		
+	}
+	
+	@FindBy(xpath="//span[text()='Group Role']")
+	WebElement grouprole;
+	
+	@FindBy(xpath="//a[text()='Create Group Type']")
+	WebElement creategrouptype;
+	
+	@FindBy(xpath="//select[@formcontrolname='roleTypeCode']")
+	WebElement roletype;
+	
+	@FindBy (xpath="//input[@id='groupTypeCode']")
+	WebElement grouprolecode;
+	
+	@FindBy(xpath="//input[@id='groupTypeName']")
+	WebElement grouprolename;
+	
+	@FindBy(xpath="//select[@formcontrolname='status']")
+	WebElement status;
+	
+	/*public List<WebElement> roleName(String s)
+	{
+		 List<WebElement> rolesrights=driver.findElements(By.xpath("//div[@_ngcontent-c2='']//div[@class='ng-untouched ng-pristine ng-valid']//label[contains(text(),'s')]"));
+		
+		return rolesrights;
+	}*/
+	@FindBy(xpath="//div[@_ngcontent-c2='']//div[@class='ng-untouched ng-pristine ng-valid']//label[contains(text(),'role')]")
+	List<WebElement> rolesrights;
+	
+	@FindBy(xpath="//button[text()='Submit']")
+	WebElement submit;
+	
+	@FindBy(xpath="//button[text()='Cancel']")
+	WebElement cancel;
+	public void GroupRole1()
+	{
+		grouprole.click();
+	}
+	
+	public void creategroup()
+	{
+		creategrouptype.click();
+	}
+	
+	public void Rtype()
+	{
+		Utility.drop(roletype, "Gera304");
+		roletype.click();
+	}
+	public void GroupRoleCode(String rolecode)
+	{
+		grouprolecode.sendKeys(rolecode);
+	}
+
+	public void GroupRoleName(String rolename)
+	{
+		grouprolename.sendKeys(rolename);
+	}
+	
+	public void userstatus()
+	{
+      Utility.drop(status, "Active");
+     
+	}
+	public void rights() throws InterruptedException
+	{
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		WebElement element = driver.findElement(By.xpath("//div[@_ngcontent-c2='']//div[@class='ng-untouched ng-pristine ng-valid']//label[contains(text(),'role')]"));
+		js.executeScript("arguments[0].scrollIntoView();", element);
+		rolesrights.get(1).click();
+		rolesrights.get(2).click();
+	}
+	public void submit() throws InterruptedException
+	{
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		WebElement element = driver.findElement(By.xpath("//button[text()='Submit']"));
+		js.executeScript("arguments[0].scrollIntoView();", element);
+		
+		submit.click();
+	}
+	public void cancel()
+	{
+		cancel.click();
+	}
+}
---
+++ DTAC-VMS/src/test/java/com/qa/datc/testscript/CreateGroupRoleTest.java
@@ -0,0 +1,45 @@
+package com.qa.datc.testscript;
+
+import java.sql.SQLException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.qa.dtac.base.BaseTest;
+import com.qa.dtac.generic.Utility;
+import com.qa.dtac.pages.CreateGroupRole;
+import com.qa.dtac.pages.LoginPage;
+
+public class CreateGroupRoleTest extends BaseTest{
+	@Test(invocationCount=1)
+	public void testgrouprole() throws InterruptedException, SQLException
+	{
+		LoginPage lp = new LoginPage(driver);
+		lp.setLoginId("123");
+		lp.setLoginPass("123");
+		lp.setLoginButtonClick();
+		//Thread.sleep(3000);
+		String rolecode=Utility.AlphaNumericString(10);
+		String rolename=Utility.getAlphaNumericString(10);
+		CreateGroupRole obj= new CreateGroupRole(driver);
+		
+		obj.GroupRole1();
+		
+		obj.creategroup();
+		obj.Rtype();
+		obj.GroupRoleCode(rolecode);
+		obj.GroupRoleName(rolename);
+		obj.userstatus();
+		
+		obj.rights();
+		
+		obj.submit();
+		System.out.println("code--------->"+rolecode);
+		System.out.println("name------>"+rolename);
+		Utility.dataBaseConnection("select * from vms_acl_group_role where group_role_name='"+rolename+"'");
+		Thread.sleep(5000);
+		String rolename_db=Utility.result.getString("group_role_name");
+        Assert.assertEquals(rolename_db, rolename);
+	}
+
+}
